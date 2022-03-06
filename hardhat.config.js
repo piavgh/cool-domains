714b6fb0969e56ccfc671c3d0d50d3318b0ff0c3
Hoang Trinh <hoang.trinhj@gmail.com>
2022-03-06 14:51:11 +0700

    chore: Verify Contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require('@nomiclabs/hardhat-waffle')
+require('@nomiclabs/hardhat-etherscan')
 require('dotenv').config()
 
 // You need to export an object to set up your config
@@ -14,5 +15,14 @@ module.exports = {
       url: process.env.MUMBAI_ALCHEMY_KEY,
       accounts: [process.env.PRIVATE_KEY],
     },
+    ropsten: {
+      url: process.env.ROPSTEN_ALCHEMY_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: process.env.ETHERSCAN_API_KEY,
   },
 }
